e4e0bc16e9ab45405b4ef0f9ad8477b2a77f5e98
Dave McCormick <dave@practicalclouds>
2012-07-13 20:38:28 +0100

    update the helper

--- pclouds-base/lib/facter/ec2_user_data_helper.sh
+++ pclouds-base/lib/facter/ec2_user_data_helper.sh
@@ -19,7 +19,6 @@ function explode_args {
 }
 
 UDARGS=`curl -s http://169.254.169.254/latest/user-data | sed -e 's/^args[ ]*:[ ]*//' | awk 'BEGIN{printit=1}($1 ~ /^---/){printit=0;getline} (printit == 1){print}'`
-#UDARGS='access_key=AKIAIAKF4TCXGGWZUSIA secret_key=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 Name=centami64 domain=ec2.practicalclouds.com platform=live bootbucket=pclouds-live-bootbucket profile=amitools amibucket=pceu1 manifest=CentOS6.2-x86_64-practicalclouds-puppet.manifest.xml amicredentials=ami-user-credentials amialiases=amialiases-centos6.2 debug'
 if [[ "$UDARGS" != "" ]]; then
         eval explode_args $UDARGS
 else
